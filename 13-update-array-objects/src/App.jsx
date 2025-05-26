f5135b303ac463dc0e2d5147175035ac19f9038f
AnhNguyen <hung000anh@gmail.com>
2025-05-26 07:43:01 +0700

    Refactor App component to manage a car list with add and remove functionality

--- 13-update-array-objects/src/App.jsx
+++ 13-update-array-objects/src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cars, setCars] = useState([]);
+  const [carMake, setCarMake] = useState("");
+  const [carYear, setCarYear] = useState(new Date().getFullYear());
+  const [carModel, setCarModel] = useState("");
+
+  function addCar() {
+    const newCar = {
+      make: carMake,
+      year: carYear,
+      model: carModel
+    };
+    setCars((cars) => [...cars, newCar]);
+
+    setCarMake(''); // Clear input field after adding
+    setCarYear(new Date().getFullYear()); // Reset to current year
+    setCarModel(''); // Clear input field after adding
+  }
+  function handleMakeChange(event) {
+    setCarMake(event.target.value);
+  }
+  function handleYearChange(event) {
+    setCarYear(event.target.value);
+  }
+  function handleModelChange(event) {
+    setCarModel(event.target.value);
+  }
+  function handleRemoveCar(index) {
+    return () => {
+      setCars((cars) => cars.filter((_, i) => i !== index));
+    };
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1>Car List</h1>
+        <ul>
+          {cars.map((car, index) => <li id={index} onClick={handleRemoveCar(index)}>{car.year} {car.make} {car.model}</li>)}
+        </ul>
+        <input type="number" value={carYear} placeholder='Enter car year' onChange={handleYearChange}/> <br />
+        <input type="text" value={carMake} placeholder='Enter car make' onChange={handleMakeChange}/> <br />
+        <input type="text" value={carModel} placeholder='Enter car model' onChange={handleModelChange}/> <br />
+        <button onClick={addCar}>Add Car</button>
     </>
   )
 }
