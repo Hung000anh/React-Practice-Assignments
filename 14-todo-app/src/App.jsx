d3334422425a12b0b33b993feafc04fc2e6cd1f5
AnhNguyen <hung000anh@gmail.com>
2025-05-27 08:51:22 +0700

    Add App component with ToDoList integration and initial setup

---
+++ 14-todo-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import React from 'react'
+import ToDoList from './ToDoList'
+import './App.css'
+function App() {
+  return (
+    <>
+      <ToDoList/>
+    </>
+  )
+}
+
+export default App
