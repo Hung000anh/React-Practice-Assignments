15d8e3e5c725a1dd6c31652f3133b3762c2e3aa3
AnhNguyen <hung000anh@gmail.com>
2025-06-01 20:20:18 +0700

    Add ComponentC with integration of ComponentD

---
+++ 17-useContext/src/ComponentC.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import ComponentD from "./ComponentD";
+function ComponentC() {
+  return (
+    <div className="box">
+      <h1>Component C</h1>
+      <ComponentD/>
+    </div>
+  );
+}
+export default ComponentC;
