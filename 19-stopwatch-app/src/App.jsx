03dd06e8bbb744b639d45a69113598dbeb7b7ced
AnhNguyen <hung000anh@gmail.com>
2025-06-05 13:39:58 +0700

    Add App component with multiple Stopwatch instances

---
+++ 19-stopwatch-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Stopwatch from './Stopwatch'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Stopwatch></Stopwatch>
+      <Stopwatch></Stopwatch>
+      <Stopwatch></Stopwatch>
+    </>
+  )
+}
+
+export default App
